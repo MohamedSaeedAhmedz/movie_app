93ecf07494045f0f9b5b386925df7b2d8d6bbe9a
MohamedSaeed <mido.ezo@yahoo.com>
2026-08-31 17:15:26 +0300

    Add splash screen view

--- lib/features/splash/presentation/splash_view.dart
+++ lib/features/splash/presentation/splash_view.dart
@@ -14,7 +14,7 @@ class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 2), () {
       if (!mounted) return;
 
       Navigator.pushReplacementNamed(context, AppRoutes.onboarding);
