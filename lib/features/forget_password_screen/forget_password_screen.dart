b71c64c7cf53501fa3151a5352cdb936358bbf9b
Mohamed-Elsayed2004 <mohamedelsayed20045@gmail.com>
2026-09-02 06:47:35 +0300

    Refactor Forget Password screen layout and add spacing for improved UI

--- lib/features/forget_password_screen/forget_password_screen.dart
+++ lib/features/forget_password_screen/forget_password_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/localization/app_localizations.dart';
 import 'package:movie_app/core/localization/app_localizations_ar.dart';
+import 'package:movie_app/core/resources/app_colors.dart';
 import 'package:movie_app/core/resources/app_icons.dart';
 import 'package:movie_app/core/resources/app_images.dart';
+import 'package:movie_app/utils/app_text_style.dart';
+import 'package:movie_app/widget/custom_button.dart';
 import 'package:movie_app/widget/custom_svg_pic.dart';
 import 'package:movie_app/widget/custom_text_form_field.dart';
 
@@ -16,6 +19,7 @@ class ForgetPasswordScreen extends StatelessWidget {
       appBar: AppBar(title: Text(l10n.forgetPasswordTitle)),
       body: SingleChildScrollView(
         child: Column(
+          spacing: 24,
           children: [
             Image.asset(MImages.forgot),
             Padding(
@@ -23,6 +27,18 @@ class ForgetPasswordScreen extends StatelessWidget {
               child: CustomTextFormField(
                 prefixIcon: CustomSvgPicture(svgPath: MIcons.emailSvg),
                 hintText: l10n.email,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+
+              child: CustomButton(
+                onTap: () {},
+                text: l10n.verify_email,
+                textStyle: AppTextStyle.font20W400.copyWith(
+                  color: MColors.black,
+                ),
+                color: MColors.yellow,
               ),
             ),
           ],
